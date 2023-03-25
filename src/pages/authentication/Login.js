bcd62e1a5eeb0fefe52ce03edf582f2f1427577b
kurtzhou1 <pandabat1@gmail.com>
2023-03-25 17:30:26 +0800

    [add] add login

--- src/pages/authentication/Login.js
+++ src/pages/authentication/Login.js
@@ -1,20 +1,40 @@
 import { Link } from 'react-router-dom';
 
 // material-ui
-import { Grid, Stack, Typography } from '@mui/material';
+import { Grid, Stack, Typography, Box } from '@mui/material';
 
 // project import
 import AuthLogin from './auth-forms/AuthLogin';
 import AuthWrapper from './AuthWrapper';
 
+import Logo from 'assets/images/logo.png';
+
 // ================================|| LOGIN ||================================ //
+const styles = {
+    paperContainer: {
+        // backgroundImage: `url(${'https://itbrief.com.au/uploads/story/2021/08/19/GettyImages-1219077605.webp'})`,
+        backgroundImage: `url(${Logo})`,
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat',
+        width: '15rem',
+        height: '5rem'
+    }
+};
 
 const Login = () => (
-    <AuthWrapper sx={{ backgroundColor: 'transparent' }}>
-        <Grid container spacing={3} sx={{ backgroundColor: 'transparent' }}>
-            <Grid item xs={12} sx={{ backgroundColor: 'transparent' }}>
-                <Stack direction="row" justifyContent="space-between" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
+    <AuthWrapper>
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
                     <Typography variant="h3">Login</Typography>
+                    <Box
+                        component="img"
+                        sx={{
+                            width: '10rem',
+                            height: 'auto'
+                        }}
+                        src={Logo}
+                    />
                     {/* <Typography component={Link} to="/register" variant="body1" sx={{ textDecoration: 'none' }} color="primary">
                         Don&apos;t have an account?
                     </Typography> */}
