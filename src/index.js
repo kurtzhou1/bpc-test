3e82ab356470847c305393eb1708b62edb8f19fd
kurtzhou1 <pandabat1@gmail.com>
2023-08-09 17:38:54 +0800

    [fix] 8/9

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import 'simplebar/src/simplebar.css';
 import { Provider as ReduxProvider } from 'react-redux';
 
 // apex-chart
-import 'assets/third-party/apex-chart.css';
+import './assets/third-party/apex-chart.css';
 
 // project import
 import App from './App';
