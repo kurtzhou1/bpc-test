677625f0c88850d583ed255016722c6cd9682994
kurtzhou1 <pandabat1@gmail.com>
2024-01-09 10:23:38 +0800

    [fix] clean cache

--- src/menu-items/creditBalanceManage.js
+++ src/menu-items/creditBalanceManage.js
@@ -3,53 +3,53 @@ import { CreditCardOutlined, TagOutlined } from '@ant-design/icons';
 
 // constant
 const icons = {
-    CreditCardOutlined,
-    TagOutlined
+  CreditCardOutlined,
+  TagOutlined,
 };
 
 // ==============================|| EXTRA PAGES MENU ITEMS ||============================== //
 
 const CreditBalanceManage = {
-    id: 'CB',
-    title: 'creditBalanceManage',
-    caption: 'Pages Caption',
-    type: 'group',
-    children: [
+  id: 'CB',
+  title: 'creditBalanceManage',
+  caption: 'Pages Caption',
+  type: 'group',
+  children: [
+    {
+      id: 'item5',
+      title: 'CreditBalance',
+      type: 'collapse',
+      url: '/CreditBalance/CreditBalanceManage',
+      icon: icons.CreditCardOutlined,
+      breadcrumbs: true,
+      children: [
         {
-            id: 'item5',
-            title: 'CreditBalance',
-            type: 'collapse',
-            url: '/CreditBalance/CreditBalanceManage',
-            icon: icons.CreditCardOutlined,
-            breadcrumbs: true,
-            children: [
-                {
-                    id: 'item51',
-                    title: 'Credit Balance管理',
-                    type: 'item',
-                    url: '/CreditBalance/CreditBalanceManage',
-                    icon: icons.TagOutlined,
-                    breadcrumbs: true
-                },
-                {
-                    id: 'item52',
-                    title: 'Credit Balance退費',
-                    type: 'item',
-                    url: '/CreditBalance/CreditBalanceRefund',
-                    icon: icons.TagOutlined,
-                    breadcrumbs: true
-                },
-                {
-                    id: 'item53',
-                    title: '退費函稿',
-                    type: 'item',
-                    url: '/CreditBalance/CreditBalanceManage',
-                    icon: icons.TagOutlined,
-                    breadcrumbs: true
-                }
-            ]
-        }
-    ]
+          id: 'item51',
+          title: 'Credit Balance管理',
+          type: 'item',
+          url: '/CreditBalance/CreditBalanceManage',
+          icon: icons.TagOutlined,
+          breadcrumbs: true,
+        },
+        {
+          id: 'item52',
+          title: 'Credit Balance退費',
+          type: 'item',
+          url: '/CreditBalance/CreditBalanceRefund',
+          icon: icons.TagOutlined,
+          breadcrumbs: true,
+        },
+        {
+          id: 'item53',
+          title: '退費函稿管理',
+          type: 'item',
+          url: '/CreditBalance/CreditBalanceManage',
+          icon: icons.TagOutlined,
+          breadcrumbs: true,
+        },
+      ],
+    },
+  ],
 };
 
 export default CreditBalanceManage;
--- src/pages/creditBalance/CreditBalanceRefund/creditBalanceDataList.jsx
+++ src/pages/creditBalance/CreditBalanceRefund/creditBalanceDataList.jsx
@@ -15,92 +15,95 @@ import { styled } from '@mui/material/styles';
 import dayjs from 'dayjs';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-        color: theme.palette.common.black,
-        paddingTop: '0.2rem',
-        paddingBottom: '0.2rem'
-    },
-    [`&.${tableCellClasses.body}`]: {
-        fontSize: 14,
-        paddingTop: '0.2rem',
-        paddingBottom: '0.2rem'
-    }
+  [`&.${tableCellClasses.head}`]: {
+    color: theme.palette.common.black,
+    paddingTop: '0.2rem',
+    paddingBottom: '0.2rem',
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+    paddingTop: '0.2rem',
+    paddingBottom: '0.2rem',
+  },
 }));
 
 const CreditBalanceDataList = ({ listInfo, cbToCn, setCbToCn }) => {
+  const handleChange = (event) => {
+    setCbToCn({ ...cbToCn, [event.target.value]: event.target.checked });
+  };
 
+  console.log();
 
-    const handleChange = (event) => {
-        setCbToCn({ ...cbToCn, [event.target.value]: event.target.checked });
-    };
-
-    console.log()
-
-    return (
-        <>
-            <TableContainer component={Paper} sx={{ maxHeight: 350 }}>
-                <Table sx={{ minWidth: 300 }} stickyHeader >
-                    <TableHead>
-                        <TableRow>
-                            <StyledTableCell align="center"></StyledTableCell>
-                            <StyledTableCell align="center">NO</StyledTableCell>
-                            <StyledTableCell align="center">CB種類</StyledTableCell>
-                            <StyledTableCell align="center">會員</StyledTableCell>
-                            <StyledTableCell align="center">發票號碼</StyledTableCell>
-                            <StyledTableCell align="center">帳單號碼</StyledTableCell>
-                            <StyledTableCell align="center">計帳段號</StyledTableCell>
-                            <StyledTableCell align="center">海纜名稱</StyledTableCell>
-                            <StyledTableCell align="center">海纜作業</StyledTableCell>
-                            <StyledTableCell align="center">剩餘金額</StyledTableCell>
-                            <StyledTableCell align="center">建立日期</StyledTableCell>
-                            <StyledTableCell align="center">摘要說明</StyledTableCell>
-                            <StyledTableCell align="center">退費金額</StyledTableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {listInfo?.map((row, id) => {
-                            return (
-                                <TableRow
-                                    key={row.CBType + row.BillingNo + row.InvoiceNo}
-                                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                >
-                                    <StyledTableCell align="center">
-                                        <Checkbox
-                                            value={row?.InvDetailID}
-                                            onChange={handleChange}
-                                            checked={cbToCn[row?.InvDetailID] || false}
-                                        />
-                                    </StyledTableCell>
-                                    <StyledTableCell align="center">{id + 1}</StyledTableCell>
-                                    <StyledTableCell align="center">{row.CBType}</StyledTableCell>
-                                    <StyledTableCell align="center">{row.PartyName}</StyledTableCell>
-                                    <StyledTableCell align="center">{row.InvoiceNo}</StyledTableCell>
-                                    <StyledTableCell align="center">{row.BillingNo}</StyledTableCell>
-                                    <StyledTableCell align="center">{row.BillMilestone}</StyledTableCell>
-                                    <StyledTableCell align="center">{row.SubmarineCable}</StyledTableCell>
-                                    <StyledTableCell align="center">{row.WorkTitle}</StyledTableCell>
-                                    <StyledTableCell align="center">{`$${handleNumber(row.CurrAmount)}`}</StyledTableCell>
-                                    <StyledTableCell align="center"> {dayjs(row.CreateDate).format('YYYY/MM/DD')}</StyledTableCell>
-                                    <StyledTableCell align="center">{row.Note}</StyledTableCell>
-                                    <StyledTableCell align="center">
-                                        <Box
-                                            sx={{
-                                                display: 'flex',
-                                                justifyContent: 'center',
-                                                '& button': { mx: { md: 0.3, lg: 0.7, xl: 1.5 }, p: 0 }
-                                            }}
-                                        >
-                                            退費金額
-                                        </Box>
-                                    </StyledTableCell>
-                                </TableRow>
-                            );
-                        })}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </>
-    );
+  return (
+    <>
+      <TableContainer component={Paper} sx={{ maxHeight: 350 }}>
+        <Table sx={{ minWidth: 300 }} stickyHeader>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="center"></StyledTableCell>
+              <StyledTableCell align="center">NO</StyledTableCell>
+              <StyledTableCell align="center">CB種類</StyledTableCell>
+              <StyledTableCell align="center">會員</StyledTableCell>
+              <StyledTableCell align="center">發票號碼</StyledTableCell>
+              <StyledTableCell align="center">帳單號碼</StyledTableCell>
+              <StyledTableCell align="center">計帳段號</StyledTableCell>
+              <StyledTableCell align="center">海纜名稱</StyledTableCell>
+              <StyledTableCell align="center">海纜作業</StyledTableCell>
+              <StyledTableCell align="center">剩餘金額</StyledTableCell>
+              <StyledTableCell align="center">建立日期</StyledTableCell>
+              <StyledTableCell align="center">摘要說明</StyledTableCell>
+              <StyledTableCell align="center">退費金額</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {listInfo?.map((row, id) => {
+              return (
+                <TableRow
+                  key={row.CBType + row.BillingNo + row.InvoiceNo + id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <StyledTableCell align="center">
+                    <Checkbox
+                      value={row?.InvDetailID}
+                      onChange={handleChange}
+                      checked={cbToCn[row?.InvDetailID] || false}
+                    />
+                  </StyledTableCell>
+                  <StyledTableCell align="center">{id + 1}</StyledTableCell>
+                  <StyledTableCell align="center">{row.CBType}</StyledTableCell>
+                  <StyledTableCell align="center">{row.PartyName}</StyledTableCell>
+                  <StyledTableCell align="center">{row.InvoiceNo}</StyledTableCell>
+                  <StyledTableCell align="center">{row.BillingNo}</StyledTableCell>
+                  <StyledTableCell align="center">{row.BillMilestone}</StyledTableCell>
+                  <StyledTableCell align="center">{row.SubmarineCable}</StyledTableCell>
+                  <StyledTableCell align="center">{row.WorkTitle}</StyledTableCell>
+                  <StyledTableCell align="center">{`$${handleNumber(
+                    row.CurrAmount,
+                  )}`}</StyledTableCell>
+                  <StyledTableCell align="center">
+                    {' '}
+                    {dayjs(row.CreateDate).format('YYYY/MM/DD')}
+                  </StyledTableCell>
+                  <StyledTableCell align="center">{row.Note}</StyledTableCell>
+                  <StyledTableCell align="center">
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        '& button': { mx: { md: 0.3, lg: 0.7, xl: 1.5 }, p: 0 },
+                      }}
+                    >
+                      退費金額
+                    </Box>
+                  </StyledTableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 };
 
 export default CreditBalanceDataList;
